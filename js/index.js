066eba090e0216d3ce45aa362e4738cf397d6b1a
xTheNightShade (Kevin Wang) <wang0071@hz.nl>
2022-11-25 09:36:32 +0100

    Added AppointmentsAPI

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const userAPI = "http://127.0.0.1:3001/users";
+const appointmentAPI = "http://127.0.0.1:3002/appointments";
 
 async function login() {
     const email = document.getElementById('email').value;
@@ -20,6 +21,31 @@ async function login() {
     });
     if (data.data.includes(email, password)){
         console.log(data.data.includes(email, password))
+    }
+
+}
+
+async function getAppointments() {
+    const response = await fetch(appointmentAPI);
+
+    //store data in json
+    let data = await response.json();
+    console.log(data)
+    const body = document.getElementById('items')
+    if (response) {
+        data.forEach(data => {
+            let listItem = document.createElement('div');
+            listItem.innerHTML = 
+            `<div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">${data.name}</h5>
+                    <p class="card-text">${data.date}</p>
+                    <p class="card-text"><small class="text-muted">${data.time}</small></p>
+                </div>
+            </div>`
+            body.append(listItem);
+        });
+        console.log(data)
     }
 
 }
