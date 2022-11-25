a31761645d096279abba778341eb85b360bc78a0
xTheNightShade (Kevin Wang) <wang0071@hz.nl>
2022-11-25 10:30:51 +0100

    Added sessionID and Logout function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const userAPI = "http://127.0.0.1:3001/users";
 const appointmentAPI = "http://127.0.0.1:3002/appointments";
 
+function init() {
+    if (sessionStorage.currentID) {
+        window.location = '/pages/homeScreen.html';
+    }
+}
+
 async function login() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value; 
@@ -16,6 +22,7 @@ async function login() {
     }
     accounts.forEach(account => {
         if (account.email === email && account.password === password){
+            sessionStorage.setItem('currentID', account.id);
             window.location = '/pages/homeScreen.html';
         }
     });
@@ -31,21 +38,29 @@ async function getAppointments() {
     //store data in json
     let data = await response.json();
     console.log(data)
+    console.log(sessionStorage.currentID)
     const body = document.getElementById('items')
     if (response) {
         data.forEach(data => {
-            let listItem = document.createElement('div');
-            listItem.innerHTML = 
-            `<div class="card mb-3">
-                <div class="card-body">
-                    <h5 class="card-title">${data.name}</h5>
-                    <p class="card-text">${data.date}</p>
-                    <p class="card-text"><small class="text-muted">${data.time}</small></p>
-                </div>
-            </div>`
-            body.append(listItem);
+            if (sessionStorage.currentID == data.owner_id){
+                let listItem = document.createElement('div');
+                listItem.innerHTML = 
+                `<div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.name}</h5>
+                        <p class="card-text">${data.date}</p>
+                        <p class="card-text"><small class="text-muted">${data.time}</small></p>
+                    </div>
+                </div>`
+                body.append(listItem);
+            }
         });
         console.log(data)
     }
 
+}
+
+function logout() {
+    sessionStorage.removeItem("currentID");
+    window.location = '../index.html'
 }
