08aa1e9c77851e81c510f4fa482cadc18a5c7be1
KevinWang2003 <wang0071@hz.nl>
2022-12-05 14:25:47 +0100

    Home, Edit and Delete Styling also added Validation to forms.

--- js/appointment.js
+++ js/appointment.js
@@ -7,25 +7,34 @@ async function getPersonalAppointments() {
     let data = await response.json();
     console.log(data);
     const body = document.getElementById('items')
+    let appointmentsStatus = document.getElementById('homescreenEvents');
+    const noContent = document.getElementById('no-content')
     if (response) {
-        data.forEach(data => {
-            if (sessionStorage.currentID == data.owner_id){
-                let listItem = document.createElement('div');
-                listItem.innerHTML = 
-                `<a href="/pages/appointmentDetails.html" onclick="StoreID(${data.id})" class="no-decoration">
-                        <div class="card mb-3">
-                            <div class="card-body">
-                                <h5 class="card-title">${data.name}</h5>
-                                    <p class="card-text">Date: ${data.date}</p>
-                                    <p class="card-text">Location: ${data.location}</p>
-                                    <p class="card-text">Price per portion: €${data.price}</p>
-                                    <p class="card-text"><small class="text-muted">Time: ${data.time}</small></p>
+        if (data.length != 0) {
+            data.forEach(data => {
+                if (sessionStorage.currentID == data.owner_id){
+
+                    // TODO: make this less janky
+                    appointmentsStatus.textContent = "You have the following appointments scheduled:";
+                    noContent.innerHTML = "";
+
+                    let listItem = document.createElement('div');
+                    listItem.innerHTML = 
+                    `<a href="/pages/appointmentDetails.html" onclick="StoreID(${data.id})" class="no-decoration">
+                            <div class="card mb-3">
+                                <div class="card-body">
+                                    <h5 class="card-title">${data.name}</h5>
+                                        <p class="card-text">Date: ${data.date}</p>
+                                        <p class="card-text">Location: ${data.location}</p>
+                                        <p class="card-text">Price per portion: €${data.price}</p>
+                                        <p class="card-text"><small class="text-muted">Time: ${data.time}</small></p>
+                                </div>
                             </div>
-                        </div>
-                    </a>`
-                body.append(listItem);
-            }
-        });
+                        </a>`
+                    body.append(listItem);
+                }
+            });
+        }
     }
 }
 
@@ -59,25 +68,28 @@ async function getAppointment() {
     const response = await fetch(appointmentAPI);
     //store data in json
     let data = await response.json();
+    const appointmentName = document.getElementById('appointment-name')
     const body = document.getElementById('information')
     if (response) {
         data.forEach(data => {
             if (sessionStorage.appointmentID == data.id){
                 let listItem = document.createElement('div');
+                let title = document.createElement('h1');
+                title.textContent = `${data.name}`;
+                
                 listItem.innerHTML = 
                 `  
                 <div class="container">
     <div class="row">
         <div class="col-lg-6 col-md-6" id="image">
-            <img src="../../resources/images/Foodsel.jpg" alt="dumb fuck" class="details-image">
+            <img src="../../resources/images/Foodsel.jpg" alt="f" class="details-image">
         </div>
         <div class="col-lg-6 col-md-6 details-info" id="info">
-            <h1>Meal: ${data.name}</h1>
             <p>Location: ${data.location}</p>
             <p>Date: ${data.date}</p>
             <p>Time: ${data.time}</p>
             <p>Price: €${data.price}</p>
-            <div class="fucked-button">
+            <div class="register-button">
               <a href="https://www.youtube.com/shorts/w1_u9uGchMY"><button type="button" class="btn btn-info">Register</button></a>
             </div>
           </div>
@@ -99,6 +111,7 @@ async function getAppointment() {
     </div>
   </div>
                 `;
+                appointmentName.append(title);
                 body.append(listItem);
             }
         });
@@ -114,20 +127,22 @@ function wipeID() {
 }
 
 async function addAppointment() {
-    const owner_id = sessionStorage.currentID;
-    const meal = document.getElementById('validationMeal').value;
-    const date = document.getElementById('validationDate').value;
-    const time = document.getElementById('validationTime').value; 
-    const location = document.getElementById('validationLocation').value;
-    const price = document.getElementById('validationPrice').value;
-    const info = document.getElementById('validationInfo').value;
-    event.preventDefault();
-    const response = await fetch(`http://127.0.0.1:3002/appointments?owner_id=${owner_id}&name=${meal}&date=${date}&time=${time}&location=${location}&price=${price}&info=${info}`, {
-        method: 'POST'
-    });
+    if (validation()) {
+        const owner_id = sessionStorage.currentID;
+        const meal = document.getElementById('validationMeal').value;
+        const date = document.getElementById('validationDate').value;
+        const time = document.getElementById('validationTime').value;
+        const location = document.getElementById('validationLocation').value;
+        const price = document.getElementById('validationPrice').value;
+        const info = document.getElementById('validationInfo').value;
+        event.preventDefault();
+        const response = await fetch(`http://127.0.0.1:3002/appointments?owner_id=${owner_id}&name=${meal}&date=${date}&time=${time}&location=${location}&price=${price}&info=${info}`, {
+            method: 'POST'
+        });
         window.location = '/pages/homeScreen.html';
-    
+
         return response
+    }
 }
 
 async function fetchOldAppointmentData() {
@@ -154,21 +169,23 @@ async function fetchOldAppointmentData() {
 }
 
 async function editAppointment() {
-    const owner_id = sessionStorage.currentID;
-    const id = sessionStorage.appointmentID;
-    const meal = document.getElementById('validationMeal').value;
-    const date = document.getElementById('validationDate').value;
-    const time = document.getElementById('validationTime').value; 
-    const location = document.getElementById('validationLocation').value;
-    const price = document.getElementById('validationPrice').value;
-    const info = document.getElementById('validationInfo').value;
-    event.preventDefault();
-    const response = await fetch(`http://127.0.0.1:3002/appointments/${id}?&name=${meal}&date=${date}&time=${time}&location=${location}&price=${price}&info=${info}`, {
-        method: 'PUT'
-    });
-        window.location = '/pages/homeScreen.html';
-    
-        return response
+    if (validation()) {
+        const owner_id = sessionStorage.currentID;
+        const id = sessionStorage.appointmentID;
+        const meal = document.getElementById('validationMeal').value;
+        const date = document.getElementById('validationDate').value;
+        const time = document.getElementById('validationTime').value; 
+        const location = document.getElementById('validationLocation').value;
+        const price = document.getElementById('validationPrice').value;
+        const info = document.getElementById('validationInfo').value;
+        event.preventDefault();
+        const response = await fetch(`http://127.0.0.1:3002/appointments/${id}?&name=${meal}&date=${date}&time=${time}&location=${location}&price=${price}&info=${info}`, {
+            method: 'PUT'
+        });
+            window.location = '/pages/homeScreen.html';
+        
+            return response
+    }
 }
 
 async function removeAppointment() {
@@ -183,20 +200,28 @@ async function removeAppointment() {
 }
 
 
-// function validation() {
+function validation() {
   
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     const forms = document.querySelectorAll('.has-validation')
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.has-validation')
+    const meal = document.getElementById('validationMeal').value;
+    const date = document.getElementById('validationDate').value;
+    const time = document.getElementById('validationTime').value; 
+    const location = document.getElementById('validationLocation').value;
+    const price = document.getElementById('validationPrice').value;
+    const info = document.getElementById('validationInfo').value;
   
-//     // Loop over them and prevent submission
-//     Array.from(forms).forEach(form => {
-//       form.addEventListener('submit', event => {
-//         if (!form.checkValidity()) {
-//           event.preventDefault()
-//           event.stopPropagation()
-//         }
+    // Loop over them and prevent submission
+    forms.forEach(form => {
+        if (form.value == null || form.value == "") {
+          event.preventDefault()
+          event.stopPropagation()
+        }
   
-//         form.classList.add('was-validated')
-//       }, false)
-//     })
-//   }
+        form.classList.add('was-validated')
+      });
+
+      if (meal !== null && meal !== "" && date !== null && date !== "" && time !== null && time !== "" && location !== null && location !== "" && price !== null && price !== "" && info !== null && info !== "") {
+        return true
+      }
+  }
--- js/register.js
+++ js/register.js
@@ -1,34 +1,39 @@
 const userAPI = "http://127.0.0.1:3001/users";
 
 async function register() {
+    if (validation()) {
+        const name = document.getElementById('validationName').value;
+        const email = document.getElementById('validationEmail').value;
+        const password = document.getElementById('validationPassword').value; 
+        const response = await fetch(`http://127.0.0.1:3001/users?username=${name}&email=${email}&password=${password}`, {
+            method: 'POST'
+        });
+    
+          window.location = '../../index.html';
+        
+          return response
+    }
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+function validation() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.has-validation');
     const name = document.getElementById('validationName').value;
     const email = document.getElementById('validationEmail').value;
     const password = document.getElementById('validationPassword').value; 
-    event.preventDefault();
-    const response = await fetch(`http://127.0.0.1:3001/users?username=${name}&email=${email}&password=${password}`, {
-        method: 'POST'
-    });
 
-      window.location = '../../index.html';
-    
-      return response
-}
+    // Loop over them and prevent submission
+    forms.forEach(form => {
+        if (form.value == null || form.value == "") {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated');
+      });
 
-// // Example starter JavaScript for disabling form submissions if there are invalid fields
-// function validation() {
-  
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     const forms = document.querySelectorAll('.has-validation')
-  
-//     // Loop over them and prevent submission
-//     Array.from(forms).forEach(form => {
-//       form.addEventListener('submit', event => {
-//         if (!form.checkValidity()) {
-//           event.preventDefault()
-//           event.stopPropagation()
-//         }
-  
-//         form.classList.add('was-validated')
-//       }, false)
-//     })
-//   };
+      if (name !== null && name !== "" && email !== null && email !== "" && password !== null && password !== "") {
+        return true
+      }
+
+  };
