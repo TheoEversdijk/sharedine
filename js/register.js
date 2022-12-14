1b61d25c45823155562d3800661b62aaeaf01848
Jesper <ex6tenze@pop-os.localdomain>
2022-12-14 09:07:31 +0100

    Initial commit

--- js/register.js
+++ js/register.js
@@ -1,39 +1,24 @@
 const userAPI = "http://127.0.0.1:3001/users";
 
 async function register() {
-    if (validation()) {
-        const name = document.getElementById('validationName').value;
-        const email = document.getElementById('validationEmail').value;
-        const password = document.getElementById('validationPassword').value; 
-        const response = await fetch(userAPI + `/register?username=${name}&email=${email}&password=${password}`, {
-            method: 'POST'
-        });
-    
-          window.location = '../../index.html';
-        
-          return response
-    }
-}
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-function validation() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.has-validation');
     const name = document.getElementById('validationName').value;
     const email = document.getElementById('validationEmail').value;
-    const password = document.getElementById('validationPassword').value; 
-
-    // Loop over them and prevent submission
-    forms.forEach(form => {
-        if (form.value == null || form.value == "") {
-          event.preventDefault()
-          event.stopPropagation()
+    const password = document.getElementById('validationPassword').value;
+    console.log("Registering...")
+    console.log(email, password)
+    const response = await fetch(userAPI + '/register', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        data: {
+            'email': email,
+            'password': password
         }
-        form.classList.add('was-validated');
-      });
+    }
+    );
 
-      if (name !== null && name !== "" && email !== null && email !== "" && password !== null && password !== "") {
-        return true
-      }
+    // window.location = '../../index.html';
 
-  };
+    return response
+};
