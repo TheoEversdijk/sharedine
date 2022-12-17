0803dc994fff0a2d601474681a3ce57a7b45ee1e
Jesper Bertijn <bert0018@hz.nl>
2022-12-17 01:16:49 +0100

    Improved test and CI

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,6 +2,30 @@
  * @jest-environment jsdom
  */
 const registerValidation = require("../js/register.js")
-test("Client-side validation", () => {
+test("Valid input", () => {
+  document.body.innerHTML =
+  `<input id="registerEmail" value="abcdef@abcdef.com"></input>`+
+  `<input id="registerPassword" value="abcdef"></input>`;
   expect(registerValidation()).toBeTruthy()
-})
+});
+
+test("Password too short", () => {
+  document.body.innerHTML =
+  `<input id="registerEmail" value="abcdef@abcdef.com"></input>`+
+  `<input id="registerPassword" value="abc"></input>`;
+  expect(registerValidation()).toBeFalsy()
+});
+
+test("Email format invalid", () => {
+  document.body.innerHTML =
+  `<input id="registerEmail" value="abcdef"></input>`+
+  `<input id="registerPassword" value="abcdef"></input>`;
+  expect(registerValidation()).toBeFalsy()
+});
+
+test("Both inputs invalid", () => {
+  document.body.innerHTML =
+  `<input id="registerEmail" value="abc"></input>`+
+  `<input id="registerPassword" value="abc"></input>`;
+  expect(registerValidation()).toBeFalsy()
+});
