f9375d1ecc97216d7de5d3b3f1c881aca43b4ff9
Jesper <ex6tenze@pop-os.localdomain>
2022-12-19 10:50:17 +0100

    Updated to new main branch

--- js/settings.js
+++ js/settings.js
@@ -8,20 +8,20 @@
 // 		cacheControl: '3600',
 // 		upsert: false
 // 	  })
-	
+
 // };
 
-// If you are using JavaScript/ECMAScript modules:
-import Dropzone from "dropzone";
+async function testAvatar() {
+  if (validation()) {
+    const avatar = document.getElementById('avatar').value;
+    const response = await fetch(userAPI + `/settings?avatar=${avatar}`, {
+      method: 'POST'
+    });
 
-// If you are using CommonJS modules:
-const { Dropzone } = require("dropzone");
+    window.location = '../../index.html';
 
-// If you are using an older version than Dropzone 6.0.0,
-// then you need to disabled the autoDiscover behaviour here:
-Dropzone.autoDiscover = false;
-
-let myDropzone = new Dropzone("#my-form");
-myDropzone.on("addedfile", file => {
-  console.log(`File added: ${file.name}`);
-});
+    return response
+  }
+  console.log(avatar);
+  // event.preventDefault();
+}
