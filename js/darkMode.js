e31cb7bb7299d94169747c1b58b414a5a94a1d5c
Kevin van Herwijnen <kvherwijnen@hotmail.com>
2022-12-21 08:36:20 +0100

    .

---
+++ js/darkMode.js
@@ -0,0 +1,7 @@
+// import Darkmode from 'darkmode-js';
+
+// new Darkmode().showWidget();
+
+// const darkmode =  new Darkmode();
+// darkmode.toggle();
+// console.log(darkmode.isActivated()) // will return true
