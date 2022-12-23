b558f0a59918f457613f201225780e61c08bc06d
Jesper Bertijn <bert0018@hz.nl>
2022-12-23 12:26:13 +0100

    Added username/pfp verification

--- js/auth.js
+++ js/auth.js
@@ -5,98 +5,130 @@ const userAPI = "http://127.0.0.1:3001/users";
  * @returns Response in JSON format
  */
 async function register() {
-  if (formValidation()) {
-    displayLoading();
 
-    // Retrieve data from Users
+  // Validate input
+  formValidation();
+  if (document.querySelector('#emailForm').checkValidity()
+    && document.querySelector('#passwordForm').checkValidity()) {
+
+    // Send request
+    displayLoading();
     const response = await fetchData(userAPI + '/register');
 
-    // Place to put the server-side validation errors
+    // Server-side validation responses
     const serverDiv = document.querySelector("#serverDiv");
-
     if (response === "The account has been created.") {
       serverDiv.innerHTML =
         `<h5>${response}</h5>` +
-        `<h5>Please check your email for a confirmation link.`
+        `<h5>Please check your email for a confirmation link.`;
     } else if ((response === "Unable to validate email address: invalid format") || (response === "Password should be at least 6 characters")) {
       serverDiv.innerHTML =
-        `<h5>${response}.</h5>`
+        `<h5>${response}.</h5>`;
     } else {
       serverDiv.innerHTML =
-        `<h5>The server has run into trouble, please try again later.</h5>`
+        `<h5>The server has run into trouble, please try again later.</h5>`;
     };
-    hideLoading()
+    hideLoading();
   };
 };
+
 
 /**
  * Log in to an existing account
  * @returns Response in JSON format
  */
 async function login() {
-  if (formValidation()) {
-    displayLoading();
+  // Validate input
+  formValidation();
+  if (document.querySelector('#emailForm').checkValidity()
+    && document.querySelector('#passwordForm').checkValidity()) {
 
-    // Retrieve data from Users
+    // Send request
+    displayLoading();
     const response = await fetchData(userAPI + '/login');
 
-    // Place to put the server-side validation errors
+    // Server-side validation responses
     const serverDiv = document.querySelector("#serverDiv");
-
     // If user exists, an object is returned
     // Store said object in session storage and move to the next page
     if (typeof (response) === 'object' && JSON.stringify(response).length !== 2) {
-      sessionStorage.setItem('userData', JSON.stringify(response))
+      sessionStorage.setItem('userData', JSON.stringify(response));
       window.location = '../pages/homeScreen.html'
 
       // Error handling for all other non-object responses
     } else if (response === "Email not confirmed") {
       serverDiv.innerHTML =
-        `<h5>Please confirm your email before logging in.</h5>`
+        `<h5>Please confirm your email before logging in.</h5>`;
     } else if (response === "Invalid login credentials") {
       serverDiv.innerHTML =
-        `<h5>These credentials do not match. Are you sure you have typed them correctly?</h5>`
+        `<h5>These credentials do not match. Are you sure you have typed them correctly?</h5>`;
     } else {
       serverDiv.innerHTML =
-        `<h5>The server has run into trouble, please try again later.</h5>`
+        `<h5>The server has run into trouble, please try again later.</h5>`;
     };
     hideLoading();
   };
 };
+
+async function verify() {
+  // Validate input
+  formValidation();
+  if (document.querySelector('#userForm').checkValidity()
+    && document.querySelector('#avatarForm').checkValidity()) {
+
+    // Send request
+    displayLoading();
+    const data = sessionStorage.getItem('userData');
+    const object = JSON.parse(data);
+    const id = object.user.id;
+    const username = document.querySelector('#userForm').value;
+    const avatar = document.querySelector('#avatarForm').value;
+    const response = await fetch(userAPI + '/verify', {
+      signal: timeOut(10).signal,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        'id': id,
+        'username': username,
+        'avatar': avatar
+      }),
+    });
+    if (response.status === 200) {
+      window.location = '../homeScreen.html'
+    } else {
+      serverDiv.innerHTML =
+        `<h5>The server has run into trouble, please try again later.</h5>`;
+    };
+  };
+};
+
+
 /**
- * Function to validate all input fields on the client side
+ * Validates all input fields on the client side
  * @returns Boolean
  */
 function formValidation() {
   // Form styling
   const forms = document.querySelectorAll('.has-validation');
   forms.forEach(form => {
-    event.preventDefault()
+    event.preventDefault();
     form.classList.add('was-validated');
   });
-
-  // Conditions to check against
-  const { email, password } = getFormData()
-  const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (
-    (email && password) !== null
-    && (email && password) !== ""
-    && email.match(mailformat) !== null
-    && password.length >= 6
-  ) {
-    return true;
-  } return false;
 };
 
 /**
- * Function to perform a POST request by supplying an email and password.
+ * Performs a POST request by supplying an email and password.
  * @param {*} url The API URL to fetch the data from
  * @returns Promise
  */
 async function fetchData(url) {
   try {
-    const { email, password } = getFormData()
+    const { email, password } = getFormData();
     const response = await fetch(url, {
+      signal: timeOut(10).signal,
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
@@ -111,7 +143,18 @@ async function fetchData(url) {
   } catch (error) {
     console.log(error);
     return error;
-  }
+  };
+};
+
+/**
+ * Sets a timeout on a fetch request
+ * @param {Number} time 
+ * @returns Abort
+ */
+const timeOut = (time) => {
+  const controller = new AbortController();
+  setTimeout(() => controller.abort(), time * 1000);
+  return controller;
 };
 
 /**
@@ -125,7 +168,7 @@ function getFormData() {
 };
 
 /**
- * Function to display the loading circle and remove the button
+ * Display the loading circle and hide the button
  */
 function displayLoading() {
   const loader = document.querySelector("#loader");
@@ -135,7 +178,7 @@ function displayLoading() {
 }
 
 /**
- * Function to hide the loading circle and return the button
+ * Hide the loading circle and display the button
  */
 function hideLoading() {
   const loader = document.querySelector("#loader");
@@ -143,3 +186,58 @@ function hideLoading() {
   loader.classList.remove("displayLoader");
   fetchButton.classList.remove("displayButton");
 }
+
+/**
+ * Verify that the user is logged in
+ */
+function checkLoggedIn() {
+  if (!sessionStorage.userData) {
+    window.location = '../index.html';
+  };
+};
+
+/**
+ * Checks if the user already has an ongoing session
+ */
+function checkUserData() {
+  if (sessionStorage.userData) {
+    window.location = './pages/homeScreen.html'
+  };
+};
+
+/**
+ * Removes user data from the session storage
+ */
+function logout() {
+  sessionStorage.removeItem("userData");
+  window.location = '../index.html';
+};
+
+/**
+ * Checks whether the logged in user has verified their account and username
+ * @returns Promise
+ * @author Jesper Bertijn
+ */
+async function checkVerified() {
+  try {
+    const object = JSON.parse(sessionStorage.userData)
+    const id = object.user.id
+    const response = await fetch(userAPI + '/checkValidated', {
+      signal: timeOut(10).signal,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        'id': id
+      }),
+    }).then((response) => response.json());
+    if (JSON.stringify(response).length > 5) {
+      window.location = './login/verify.html'
+    };
+  } catch (error) {
+    console.log(error);
+    return error;
+  };
+};
